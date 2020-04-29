5fedf79a512bdff17ccfbc7398f42ba00ae45869
leoxiaojw <leoxiaojw@gmail.com>
2020-04-29 16:51:39 +0800

    日志引擎开发

--- log_model/log_operation.py
+++ log_model/log_operation.py
@@ -1,3 +1,22 @@
 # -*- coding:utf-8 -*-
 __author__ = "leo"
 
+import logging
+
+from log_model.properties_utils import Properties
+
+
+def set_log_config():
+    pro = Properties("log.properties").get_properties()
+
+    log_config = {
+        "filename": pro["filename"],
+        "level": pro["level"]
+    }
+
+    logging.basicConfig(**log_config)
+
+
+if __name__ == '__main__':
+    set_log_config()
+    logging.info("info log")
---
+++ log_model/logging_setting.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+__author__ = "leo"
+
+import logging.config
+import logging
+import os
+
+
+def get_logger(name="myAutoTest"):
+    config_log = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logging.conf")
+    logging.config.fileConfig(config_log)
+    logger = logging.getLogger(name)
+
+    return logger
+
+
+logger_test = get_logger()
+logger_test.info("info log")
